5427ea4dd5f7fad587f6329584b421d1cbf03cf4
Akash <rauniyarakash144@gmail.com>
2026-01-30 11:20:23 +0530

    h

--- 29-01-26/server.js
+++ 29-01-26/server.js
@@ -3,7 +3,10 @@ const http = require("http");
 const PORT = 3000;
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "Content-Type": "text/plain" });
+  res.writeHead(200, {
+    "Content-Type": "text/plain",
+    "Access-Control-Allow-Origin": "*", // IMPORTANT for React
+  });
   res.end("Server is running!");
 });
 
