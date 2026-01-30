4aa1d9be2620f5d5d5a115387a86f3fece1047cf
Akash <rauniyarakash144@gmail.com>
2026-01-30 11:21:42 +0530

    n

--- 29-01-26/App.jsx
+++ 29-01-26/App.jsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 
 function App() {
-  const [message, setMessage] = useState("Starting server...");
+  const [message, setMessage] = useState("Checking server status...");
 
   useEffect(() => {
-    const timerId = setTimeout(() => {
-      setMessage("Server is running!");
-    }, 6000);
-
-    
-    return () => clearTimeout(timerId);
+    fetch("http://localhost:3000")
+      .then((res) => res.text())
+      .then((data) => setMessage(data))
+      .catch(() => setMessage("Server is NOT running"));
   }, []);
 
   return (
